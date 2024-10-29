7d7cfad3f4e18e91d12e1899597893f191c8d3ae
MrHyaena <dolezalmartin131@gmail.com>
2024-10-29 18:37:18 +0100

    Dropdown built

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./styles.css";
+
+const dropdown = document.querySelector("#dropdown");
+const nav = document.querySelector("#nav");
+const openDropdown = document.querySelector("openDropdown");
+
+nav.removeChild(dropdown);
+
+let index = 0;
+
+document.getElementById("openDropdown").addEventListener("click", () => {
+  if (index == 0) {
+    index = 1;
+    nav.appendChild(dropdown);
+  } else if (index == 1) {
+    index = 0;
+    nav.removeChild(dropdown);
+  }
+});
